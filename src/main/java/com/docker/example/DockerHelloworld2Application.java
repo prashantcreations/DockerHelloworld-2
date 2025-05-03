d3e05159773c0d6c9062403664c2ee4907d59ebe
Prashant Tiwari <prashanttiwari276@gmail.com>
2025-05-03 13:23:53 +0530

    first commit

---
+++ src/main/java/com/docker/example/DockerHelloworld2Application.java
@@ -0,0 +1,13 @@
+package com.docker.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerHelloworld2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerHelloworld2Application.class, args);
+	}
+
+}
